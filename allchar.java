502c75c349abcb7e9ab8b62abaafeb18152d071c
Bulbul <bulbulbanjriya123@gmail.com>
2026-05-24 14:26:15 +0530

    first commit

---
+++ allchar.java
@@ -0,0 +1,12 @@
+package armycoder.com;
+
+public class allchar {
+	public static void main(String args[])
+	{
+		int i=0;
+		do {
+			System.out.println(i+"="+(char)i);
+			i++;
+		}while(i<=127);
+	}
+}
