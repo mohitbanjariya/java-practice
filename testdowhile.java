4a35fe1e58a39d3dc25f54ac1f18850a997b5bc0
mohit <banjariyamohit@gmail.com>
2026-05-25 19:46:29 +0530

    testdowhile

---
+++ testdowhile.java
@@ -0,0 +1,14 @@
+package armycoder.com;
+
+public class testdowhile {
+	public static void main(String arr[])
+	{
+		int i=1;
+		do {
+			System.out.println(i);
+			i++;
+		}
+		while(i<=5);
+	}
+
+}
