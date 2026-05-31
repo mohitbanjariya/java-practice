0519b4cac33362f149ca38243f38c1586e1b3802
mohit <banjariyamohit@gmail.com>
2026-05-31 17:54:17 +0530

    onedimension

--- onedimensionarr.java
+++ onedimensionarr.java
@@ -1,6 +1,10 @@
 package armycoder.com;
 
+
+
 public class onedimensionarr {
+	private static int j;
+
 	public static void main(String arr[])
 	{
 		//1st way to create one dimension array = compile time memory
@@ -17,9 +21,63 @@ public class onedimensionarr {
 		//3rd way to create one dimension array = run time memory memory allocation
 		int z[] = new int[] {12,34,23,34,56};
 		
-		for(int i=0; i<=5; i++)
+//		for(int i=0; i<=5; i++)
+//		{
+//			System.out.println(z[i]);
+//		}
+		System.out.println("-----------------------");
+		
+		//1st way to create 2dimension array = compile time memory
+		int arr1 [][] = {
+				        {12,34,5,77,89},
+				        {13,45,66,78,97},
+				        {11,14,15,56,67}
+				   
+		            };
+		System.out.println(arr1);
+		System.out.println(arr1.length);
+		System.out.println(arr1[0][0]);
+    	System.out.println(arr1[0][1]);
+    	System.out.println(arr1[1][1]);
+    	System.out.println(arr1[2][2]);
+    	System.out.println("--------------------------");
+    	
+//    	for(int i=0; i<=arr1.length; i++)
+//    	{
+//    		for(int j=0; j<=arr1.length; j++)
+//    		{
+//    			System.out.println(arr1[i][j]);
+//    		}
+//    		System.out.print("\n");
+//    	}
+    	
+		//2nd way to create 2dimension array = run time memory memory allocation
+
+		int[][] arr2 = new int [3][4];
+		arr2[0][0] = 12;
+		arr2[0][1] = 11;
+		arr2[0][2] = 13;
+		arr2[0][3] = 14;
+		
+		arr2[1][0] = 16;
+		arr2[1][1] = 15;
+		arr2[1][2] = 17;
+		arr2[1][3] = 18;
+		
+		arr2[2][0] = 19;
+		arr2[2][1] = 11;
+		arr2[2][2] = 13;
+		arr2[2][3] = 14;
+		
+		
+		
+		for(int i=0; i<=arr2.length; i++)
 		{
-			System.out.println(z[i]);
+			for(int j=0; j<=arr2.length; j++)
+			{
+				System.out.println(arr2[i][j]);
+			}
+			System.out.print("\n");
 		}
 		
 		
