c63ab3944b12d7cd6d26000acdf0c369ee89b4df
mohit <banjariyamohit@gmail.com>
2026-06-01 22:42:33 +0530

    nestedloop

--- nestedlop1.java
+++ nestedlop1.java
@@ -2,6 +2,8 @@ package armycoder.com;
 
 public class nestedlop1 {
 
+	private static char j11;
+
 	public static void main(String arr[])
 	{
 		for(int i=1; i<=5; i++)
@@ -16,6 +18,75 @@ public class nestedlop1 {
 			}
 			System.out.println();
 		}
+		
+		System.out.println("-----------------------");
+		for(int i=1; i<=5; i++)
+		{
+			for(int k=4-i; k>=4; k--)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println("-----------------------");
+		for(int i=1; i<=5; i++)
+		{
+			
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		System.out.println("-----------------------");
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+		System.out.println("-----------------------");
+		int j = 1;
+		for(int i=5; i>=1; i--)
+		{
+			for( j=1; j<=i; j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println("-----------------------");
+		for(char i='A'; i<='E'; i++)
+		{
+			
+			for(char j1='A'; j1<=i; j1++)
+			{
+				
+				System.out.print(j1);
+			}
+			System.out.println();
+		}
+		
+		System.out.println("-----------------------");
+		for(char i='A'; i<='E'; i++)
+		{
+			
+			for(char j1='A'; j1<=i; j1++)
+			{
+				
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+		
+		System.out.println("-----------------------");
+		
 	}
 }
 		
